f30449f9a4216449bd5c98ac765bc37ed7717afc
Kee <121441909@qq.com>
2017-07-10 23:02:51 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path = require("path");
+
 module.exports = {
   entry: "./index.js",
   output: {
-    path: __dirname + "./dist",
+    path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js"
   },
   module: {
